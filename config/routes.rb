a77963a3739da6c8d550a815202de1cd7333ba35
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-08-21 19:43:56 -0400

    addes user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts
+  devise_for :members, :controllers => { registrations: 'registrations'}
+  resources :posts, only: [:new, :create, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "posts#index"
 end
